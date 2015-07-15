c17d4e7ee35591e1d5ac5a956aabc32b1b4cbdb9
andykisaragi <andy.4705@gmail.com>
2015-07-15 10:19:20 +0100

    paralleax improvements, add template file

--- keyimage_para.js
+++ keyimage_para.js
@@ -1,17 +1,9 @@
 (function ($) {
 	$(document).ready(function(){
-
-
 		$('.keyimage-para-wrapper img').css("position","absolute").css("bottom","0"); 
-
 		$(window).on('scroll touchmove', function(e) {
-
 			var scrolltop = ($(window).scrollTop());
-
 			var margin = Math.floor(scrolltop);
-
-
-
 			/*
 			if(navigator.userAgent.match(/iPad/i) != null){
 		      var scrolltop = ($("#main-container").scrollTop());
@@ -21,8 +13,6 @@
 		    */
 			//$('.keyimage-para-wrapper img').css("margin-top",(margin / 2) + "px"); 
 			$('.keyimage-para-wrapper img').css("bottom","-" + (margin / 2) + "px"); 
-
 		});
-
 	});
 })(jQuery);
